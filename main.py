a39cff85a095aa400b3b50da55edf9e431fb2827
Yevfimii <yevfimiiloboda1276@gmail.com>
2025-01-19 18:34:22 +0200

    irst part subjects and comments

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ app.include_router(subjects.router)
 app.include_router(tasks.router)
 app.include_router(enrollments.router)
 app.include_router(notifications.router)
+from fastapi import FastAPI, Depends, HTTPException
+from models import Base
+from database import engine
+from routes import subjects
+
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(subjects.router)
