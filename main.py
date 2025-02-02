8d81296cd4045c7fd1b17517c0dae1db6d9a148e
JetCraker <poop402r@gmail.com>
2025-02-02 18:20:26 +0200

    try to fix 4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from database import init_db, recreate_database
 from routes import auth, subjects, tasks, enrollments, notifications
 
-app = FastAPI(debug=True)
+app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
@@ -19,3 +19,4 @@ app.include_router(subjects.router)
 app.include_router(tasks.router)
 app.include_router(enrollments.router)
 app.include_router(notifications.router)
+app.include_router(chats.router)
