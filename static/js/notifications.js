24df79e2bc6cfa6f523aaadc13bf22a88327eafa
JetCraker <poop402r@gmail.com>
2025-02-12 20:06:22 +0200

    поміняв посилання на сокет

--- static/js/notifications.js
+++ static/js/notifications.js
@@ -5,7 +5,7 @@ class NotificationManager {
     }
 
     connect() {
-        this.socket = new WebSocket(`ws://localhost:8000/notifications/ws/${this.userId}`);
+        this.socket = new WebSocket(`ws://127.0.0.1:8000/notifications/ws/${this.userId}`);
         
         this.socket.onmessage = (event) => {
             const message = JSON.parse(event.data);
